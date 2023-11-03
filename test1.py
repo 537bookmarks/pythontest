5fd16c1510fe89ef26493377672b6a8fede794b8
marksbook537 <bookmarks537a@gmail.com>
2023-11-04 04:10:31 +0800

    git init

---
+++ test1.py
@@ -0,0 +1,41 @@
+import os
+
+print(os.getcwd())
+def sum(a,b):
+    return a+b
+
+print(sum(1,2))
+
+class A:
+    def __init__(self,a,b) -> None:
+        self.a =a
+        self.b = b
+    def sub(self):
+        return self.a-self.b
+    
+    
+    
+A1 = A(22,333)
+print(A1.a,A1.b)
+
+print(A1.sub())
+
+
+import random
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
